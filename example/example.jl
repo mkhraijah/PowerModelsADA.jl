62d9a3d2b9dd626d08171ffbda109ffb351176e9
Mohannad Alkhraijah <mkhraijah@gmail.com>
2022-07-23 21:38:38 -0400

    change package name

--- example/example.jl
+++ example/example.jl
@@ -1,32 +1,31 @@
 
-path = joinpath(@__DIR__,"..")
-cd(path)
 using Pkg
 Pkg.activate(".")
 using Ipopt
+using JuMP
+## Import PMADA package
 
-##
-
-include("../src/DistributedPowerModels.jl")
-DPM = DistributedPowerModels
+using PMADA
 
 ## Read case with partition file and return dictionary of the paritioned case
 case_path = "test/data/case14.m"
-data = DPM.parse_file(case_path)
+data = PMADA.parse_file(case_path)
 
 
-## Settings
-alpha = 1000
+## Settings and
 max_iteration = 1000
 tol = 1e-4
 optimizer = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "print_level"=>0)
 
+##  Parameter and Power Flow Model selection
+alpha = 1000
+model_type = PMADA.DCPPowerModel
+
 
 ##  Distributed algorithm settings
-model_type = DPM._PM.DCPPowerModel
 
-data_area = DPM.run_dopf_admm(data, model_type, optimizer, alpha=1000, tol=1e-4, max_iteration=1000, verbose = true)
+data_area = PMADA.run_dopf_admm(data, model_type, optimizer, tol=1e-4, max_iteration=1000, verbose = true, alpha=1000)
 
-data_area = DPM.run_dopf_app(data, model_type, optimizer, alpha=1000, tol=1e-4, max_iteration=1000, verbose = true)
+data_area = PMADA.run_dopf_app(data, model_type, optimizer, tol=1e-4, max_iteration=1000, verbose = true, alpha=1000)
 
-data_area = DPM.run_dopf_atc(data, model_type, optimizer, alpha=1.05, tol=1e-4, max_iteration=1000, verbose = true)
+data_area = PMADA.run_dopf_atc(data, model_type, optimizer, tol=1e-4, max_iteration=1000, verbose = true, alpha=1.05)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using DistributedPowerModels
+using PMADA
 
 import JuMP
 import HiGHS
@@ -11,20 +11,19 @@ using Test
 milp_solver = JuMP.optimizer_with_attributes(HiGHS.Optimizer, "output_flag"=>false)
 nlp_solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "tol"=>1e-6, "print_level"=>0)
 
-const DPM = DistributedPowerModels
 PowerModels.silence()
 
 ## load test systems
-data_14 =  DPM.parse_file("../test/data/case14.m")
-data_RTS =  DPM.parse_file("../test/data/case_RTS.m")
-data_300 =  DPM.parse_file("../test/data/case300.m")
-data_300_3 =  DPM.parse_file("../test/data/case300_3areas.m")
+data_14 =  PMADA.parse_file("../test/data/case14.m")
+data_RTS =  PMADA.parse_file("../test/data/case_RTS.m")
+data_300 =  PMADA.parse_file("../test/data/case300.m")
+data_300_3 =  PMADA.parse_file("../test/data/case300_3areas.m")
 
-@testset "DistributedPowerModels" begin
+@testset "PMADA" begin
 
 ## assign area test
     @testset "assign area to busses" begin
-        DPM.assign_area!(data_300, "../test/data/case300_8areas.csv")
+        PMADA.assign_area!(data_300, "../test/data/case300_8areas.csv")
         area_count = [65, 35, 33, 38, 43, 44, 22, 20]
         test_count = [count(c -> c["area"]  == k, [bus for (i,bus) in data_300["bus"]]) for k in 1:8]
         @test test_count == area_count
@@ -33,7 +32,7 @@ data_300_3 =  DPM.parse_file("../test/data/case300_3areas.m")
 ## decompose system test
     @testset "decmpose system into subsystems" begin
         @testset "case300" begin
-            data_area = DPM.decompose_system(data_300_3)
+            data_area = PMADA.decompose_system(data_300_3)
             bus_size = [169, 83, 69]
             gen_size = [41, 27, 22]
             branch_size = [216, 117, 90]
@@ -55,7 +54,7 @@ data_300_3 =  DPM.parse_file("../test/data/case300_3areas.m")
 ## paritiotioning test
     @testset "partition system using spectral clustering" begin
         @testset "case_RTS" begin
-            DPM.partition_system!(data_RTS, 3, init=[101, 201, 301])
+            PMADA.partition_system!(data_RTS, 3, init=[101, 201, 301])
             test_count = [count(c -> c["area"]  == k, [bus for (i,bus) in data_RTS["bus"]]) for k in 1:3]
             test_count_24 = count(==(24),test_count)
             test_count_25 = count(==(25),test_count)
@@ -66,41 +65,41 @@ data_300_3 =  DPM.parse_file("../test/data/case300_3areas.m")
 
 ## ADMM test
     @testset "admm algorithm with DC power flow" begin
-        data_area = DPM.run_dopf_admm(data_14, "DC", milp_solver; alpha=1000, tol=1e-2, max_iteration=1000, verbose = false)
-        dist_cost = DPM.calc_dist_gen_cost(data_area)
+        data_area = PMADA.run_dopf_admm(data_14, PMADA.DCPPowerModel, milp_solver; alpha=1000, tol=1e-2, max_iteration=1000, verbose = false)
+        dist_cost = PMADA.calc_dist_gen_cost(data_area)
         @test abs((dist_cost - 7642.59)/7642.59 *100) <= 1e0
 
     end
 
     @testset "admm algorithm with AC power flow" begin
-        data_area = DPM.run_dopf_admm(data_14, "AC", nlp_solver; alpha=1000, tol=1e-2, max_iteration=1000, verbose = false)
-        dist_cost = DPM.calc_dist_gen_cost(data_area)
+        data_area = PMADA.run_dopf_admm(data_14, PMADA.ACPPowerModel, nlp_solver; alpha=1000, tol=1e-2, max_iteration=1000, verbose = false)
+        dist_cost = PMADA.calc_dist_gen_cost(data_area)
         @test abs((dist_cost - 8081.52)/8081.52 *100) <= 1e0
     end
 
     ## ATC test
     @testset "atc algorithm with DC power flow" begin
-        data_area = DPM.run_dopf_atc(data_14, "DC", milp_solver; alpha=1.1, tol=1e-2, max_iteration=1000, verbose = false)
-        dist_cost = DPM.calc_dist_gen_cost(data_area)
+        data_area = PMADA.run_dopf_atc(data_14, PMADA.DCPPowerModel, milp_solver; alpha=1.1, tol=1e-2, max_iteration=1000, verbose = false)
+        dist_cost = PMADA.calc_dist_gen_cost(data_area)
         @test abs((dist_cost - 7642.59)/7642.59 *100) <= 1e0
     end
 
     @testset "atc algorithm with SOC relaxation of power flow" begin
-        data_area = DPM.run_dopf_atc(data_14, "SOC", nlp_solver; alpha=1.1, tol=1e-2, max_iteration=1000, verbose = false)
-        dist_cost = DPM.calc_dist_gen_cost(data_area)
+        data_area = PMADA.run_dopf_atc(data_14, PMADA.SOCWRPowerModel, nlp_solver; alpha=1.1, tol=1e-2, max_iteration=1000, verbose = false)
+        dist_cost = PMADA.calc_dist_gen_cost(data_area)
         @test abs((dist_cost - 8075.12)/8075.12 *100) <= 1e-1
     end
 
     ## APP test
     @testset "app algorithm with DC power flow" begin
-        data_area = DPM.run_dopf_app(data_14, "DC", milp_solver; alpha=1000, tol=1e-2, max_iteration=1000, verbose = false)
-        dist_cost = DPM.calc_dist_gen_cost(data_area)
+        data_area = PMADA.run_dopf_app(data_14, PMADA.DCPPowerModel, milp_solver; alpha=1000, tol=1e-2, max_iteration=1000, verbose = false)
+        dist_cost = PMADA.calc_dist_gen_cost(data_area)
         @test abs((dist_cost - 7642.59)/7642.59 *100) <= 1e0
     end
 
     @testset "app algorithm with ACR power flow" begin
-        data_area = DPM.run_dopf_app(data_14, "ACR", nlp_solver; alpha=1000, tol=1e-2, max_iteration=1000, verbose = false)
-        dist_cost = DPM.calc_dist_gen_cost(data_area)
+        data_area = PMADA.run_dopf_app(data_14, PMADA.ACRPowerModel, nlp_solver; alpha=1000, tol=1e-2, max_iteration=1000, verbose = false)
+        dist_cost = PMADA.calc_dist_gen_cost(data_area)
         @test abs((dist_cost - 8081.52)/8081.52 *100) <= 1e-1
     end
 end
