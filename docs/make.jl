37ca87648a4673bbc9ad1673d9f2c415a26c1c46
Mohannad Alkhraijah <mkhraijah@gmail.com>
2022-09-01 22:50:54 -0400

    correct docs

--- docs/make.jl
+++ docs/make.jl
@@ -15,20 +15,18 @@ makedocs(
         "Manual" => [
             "Quick Start Guide" => "quickguide.md",
             "Data Structure" => "data_structure.md",
-            "Problem Formulation and Algorithm", 
+            "Problem Specification and Algorithm Selection" => "formulation.md" , 
             "Distributed Algorithms" => [
-                "ADMM" => "admm.md"
-                "ATC" => "atc.md"
-                "APP" => "app.md"
+                "ADMM" => "admm.md",
+                "ATC" => "atc.md",
+                "APP" => "app.md",
                 "ALADEN" => "aladen.md"
             ],
             "Utilities" => "utilities.md"
         ],
         "Tutorials" => "tutorial.md",
         "Library" => "library.md"
-        
     ]
-
 )
 
 deploydocs(
---
+++ docs/src/specification.md
@@ -0,0 +1,11 @@
+# Problem Specification and Algorithm Selection 
+
+
+## Power Flow Formulation 
+
+
+## Optimization Solver
+
+
+## Distirbuted Algorithm 
+
